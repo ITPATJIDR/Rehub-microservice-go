480fa6a7108c37496e15c89336462951442fac76
ITPATJIDR <ittipatjitrada@gmail.com>
2023-11-22 17:13:37 +0700

    fix: uploadFile add filePath and create folder if not exist

--- controllers/uploadControllers.go
+++ controllers/uploadControllers.go
@@ -1,65 +1,104 @@
 package uploadController
 
 import (
-	"Rehub_Microservice/model"
-  "os"
+	fileModel "Rehub_Microservice/model"
+	"crypto/rand"
+	"encoding/hex"
+	"io"
+	"io/ioutil"
 	"net/http"
-  "io"
+	"os"
 
 	"github.com/gin-gonic/gin"
 )
 
 func UploadFile(c *gin.Context) {
-  file, err := c.FormFile("file")  
-  if err != nil {
-    c.JSON(http.StatusBadRequest, gin.H{
-      "error": err.Error(),
-    })
-    return
-  }
 
-  filename := "uploads/" + file.Filename
-  err = c.SaveUploadedFile(file,filename)
-  if err != nil {
-    c.JSON(http.StatusInternalServerError, gin.H{
-      "error": err.Error(),
-    })
-    return
-  }
+	var uploadData fileModel.UploadFileSturct
 
-  c.JSON(http.StatusOK, gin.H{
-    "message": "File uploaded successfully",
-    "filename": filename,
-  })
+	bytes := make([]byte, 10)
+	_, err := rand.Read(bytes)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": err.Error(),
+		})
+	}
+
+	if err := c.ShouldBindJSON(&uploadData); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+	}
+
+	if _, err := os.Stat(`C:\Users\CoSI-Lenovite\Desktop\code\Rehub-microservice-go\uploads\` + uploadData.Foldername); os.IsNotExist(err) {
+
+		err := os.MkdirAll(`C:\Users\CoSI-Lenovite\Desktop\code\Rehub-microservice-go\uploads\`+uploadData.Foldername, os.ModePerm)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"error": err.Error(),
+			})
+		}
+
+		filename := `C:\Users\CoSI-Lenovite\Desktop\code\Rehub-microservice-go\uploads\` + uploadData.Foldername + "/" + uploadData.Foldername + "_" + hex.EncodeToString(bytes) + ".json"
+		err = ioutil.WriteFile(filename, []byte(uploadData.Physicalrawreport), os.ModePerm)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{
+			"message":  "File uploaded successfully",
+			"Filepath": filename,
+		})
+
+	} else if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": err.Error(),
+		})
+
+	} else {
+		filename := `C:\Users\CoSI-Lenovite\Desktop\code\Rehub-microservice-go\uploads\` + uploadData.Foldername + "/" + uploadData.Foldername + "_" + hex.EncodeToString(bytes) + ".json"
+		err := ioutil.WriteFile(filename, []byte(uploadData.Physicalrawreport), os.ModePerm)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+		c.JSON(http.StatusOK, gin.H{
+			"message":  "File uploaded successfully",
+			"Filepath": filename,
+		})
+	}
 }
 
 func DownloadFile(c *gin.Context) {
-  var filePath fileModel.FileSturct
+	var filePath fileModel.FileSturct
 
-  if err:= c.ShouldBindJSON(&filePath); err != nil {
-    c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-  }
+	if err := c.ShouldBindJSON(&filePath); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+	}
 
-  file, err := os.Open(filePath.Filepath)
-  if err != nil {
-    c.JSON(http.StatusInternalServerError, gin.H{"error": "Error opening file"})
-    return
-  }
-  defer file.Close()
+	file, err := os.Open(filePath.Filepath)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Error opening file"})
+		return
+	}
+	defer file.Close()
 
-  c.Header("Content-Description", "File Transfer")
-  c.Header("Content-Transfer-Encoding", "binary")
-  c.Header("Content-Disposition", "attachment; filename="+filePath.Filepath)
-  c.Header("Content-Type", "application/octet-stream")
-  c.Header("Expires", "0")
-  c.Header("Cache-Control", "must-revalidate")
-  c.Header("Pragma", "public")
+	c.Header("Content-Description", "File Transfer")
+	c.Header("Content-Transfer-Encoding", "binary")
+	c.Header("Content-Disposition", "attachment; filename="+filePath.Filepath)
+	c.Header("Content-Type", "application/octet-stream")
+	c.Header("Expires", "0")
+	c.Header("Cache-Control", "must-revalidate")
+	c.Header("Pragma", "public")
 
-  _, err = io.Copy(c.Writer, file)
-      if err != nil {
-        c.JSON(http.StatusInternalServerError, gin.H{"error": "Error copying file content to response"})
-        return
-      }
+	_, err = io.Copy(c.Writer, file)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Error copying file content to response"})
+		return
+	}
 
-  c.Writer.Flush()
+	c.Writer.Flush()
 }
--- model/fileModel.go
+++ model/fileModel.go
@@ -4,3 +4,8 @@ package fileModel
 type FileSturct struct {
   Filepath string `json:"Filepath" binding:"required"` 
 }
+
+type UploadFileSturct struct {
+  Foldername string `json:"Foldername" binding:"required"`
+  Physicalrawreport string `json:"Physicalrawreport" binding:"required"`
+}
